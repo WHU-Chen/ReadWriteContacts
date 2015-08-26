db11417d959b1ab93e249f2bb0cc202d0365f368
Remch <hai_bu_hui@163.com>
2015-08-26 17:34:07 +0800

    加入GPS定位功能

--- app/src/main/java/com/example/administrator/readwritecontacts/MainFaceActivity.java
+++ app/src/main/java/com/example/administrator/readwritecontacts/MainFaceActivity.java
@@ -7,15 +7,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -28,8 +35,17 @@ public class MainFaceActivity extends Activity {
 
     List<String> listContact=new ArrayList<String>();
 
+    private TextView locationText;
+    private Location lastLocation;
     private IntentFilter sendFilter;
     private  SendStatusReceiver sendStatusReceiver;
+    private EditText editText;
+    public LocationManager getLocationManager() {
+        return locationManager;
+    }
+
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +54,21 @@ public class MainFaceActivity extends Activity {
         Button btnSend= (Button) findViewById(R.id.btnSend);
         Button btnEdit= (Button) findViewById(R.id.btnEdit);
         Button btnSave= (Button) findViewById(R.id.btnSave);
-        final EditText editText= (EditText) findViewById(R.id.etText);
+        locationText= (TextView) findViewById(R.id.locationText);
+        lastLocation=null;
+        editText= (EditText) findViewById(R.id.etText);
+
+        initialLocation();
 
         sendFilter=new IntentFilter();
         sendFilter.addAction("SENT_SMS_ACTION");
         sendStatusReceiver=new SendStatusReceiver();
         registerReceiver(sendStatusReceiver,sendFilter);
 
+        LocationManager locationManager;
+
         SharedPreferences pref=getSharedPreferences("data",MODE_PRIVATE);
-        editText.setText(pref.getString("editText","Help!"));
+        editText.setText(pref.getString("editText",getString(R.string.initialSms)));
         readContacts();
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,25 +87,55 @@ public class MainFaceActivity extends Activity {
                 Toast.makeText(MainFaceActivity.this,"保存成功",Toast.LENGTH_SHORT).show();
             }
         });
+
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SmsManager smsManager = SmsManager.getDefault();
-                Intent sentIntent = new Intent("SENT_SMS_ACTION");
-                PendingIntent pi=PendingIntent.getBroadcast(MainFaceActivity.this,0,sentIntent,0);
-                for(String i:listContact) {
-                    smsManager.sendTextMessage(
-                            i,
-                            null,
-                            editText.getText().toString(),
-                            pi,
-                            PendingIntent.getBroadcast(MainFaceActivity.this, 0, sentIntent, 0));
-                }
-
+                sendSMS();
             }
         });
     }
 
+    public void sendSMS() {
+        String location="";
+        lastLocation=getLocationManager().getLastKnownLocation(provider);
+        if(lastLocation!=null){
+            location="\n我的位置：纬度"+lastLocation.getLatitude()+" 经度"+lastLocation.getLongitude();
+            Log.d("MainFaceActivity","获取到位置信息");
+        }else Log.d("MainFaceActivity","未获取到位置信息");
+        SmsManager smsManager = SmsManager.getDefault();
+        Intent sentIntent = new Intent("SENT_SMS_ACTION");
+        PendingIntent pi=PendingIntent.getBroadcast(MainFaceActivity.this,0,sentIntent,0);
+        for(String i:listContact) {
+            smsManager.sendTextMessage(
+                    i,
+                    null,
+                    editText.getText().toString()+location,
+                    pi,
+                    PendingIntent.getBroadcast(MainFaceActivity.this, 0, sentIntent, 0));
+        }
+
+    }
+
+    private  void initialLocation(){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> providersList=locationManager.getProviders(true);
+        if(providersList.contains(LocationManager.GPS_PROVIDER)){
+            provider=LocationManager.GPS_PROVIDER;
+            Log.d("MainFaceActivity","获得GPS_PROVIDER");
+        }else if(providersList.contains(LocationManager.NETWORK_PROVIDER)){
+            provider=LocationManager.NETWORK_PROVIDER;
+            Log.d("MainFaceActivity","获得NETWORK_PROVIDER");
+        }else {
+            Toast.makeText(this,"No location provider",Toast.LENGTH_SHORT).show();
+            Log.d("MainFaceActivity", "未获取PROVIDER");
+            provider=null;
+        }
+        Location location=locationManager.getLastKnownLocation(provider);
+        if(location!=null)
+            locationText.setText(getLocationText(location));
+        locationManager.requestLocationUpdates(provider,5000,10,locationListener);
+    }
     @Override
     protected void onResume() {
         super.onResume();
@@ -94,6 +146,7 @@ public class MainFaceActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(sendStatusReceiver);
+        locationManager.removeUpdates(locationListener);
     }
 
     private void readContacts(){
@@ -134,4 +187,28 @@ public class MainFaceActivity extends Activity {
         }
     }
 
+    LocationListener locationListener =new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            locationText.setText(getLocationText(location));
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            locationText.setText(getLocationText(locationManager.getLastKnownLocation(s)));
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private String getLocationText(Location location) {
+        return new String("latitude:"+location.getLatitude()+"\nlongtitude:"+location.getLongitude());
+    }
 }
